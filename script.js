4bcf8729bdbe5559e70550704e88ff43662d6b90
AnnKomissarova <nnkmssrv@gmail.com>
2022-11-03 16:51:09 +0300

    hometask - 10

--- script.js
+++ script.js
@@ -5,4 +5,40 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // Здесь твой код
+
+  const sendBtn = document.querySelector("#button");  
+
+  sendBtn.addEventListener('click', () => {
+    fetch(`http://46.21.248.81:3001/user`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer: AnnKomissarova'
+      },
+
+      body: JSON.stringify({
+        "name": document.querySelector("#name").value,
+        "secondName": document.querySelector("#secondName").value,
+        "email": document.querySelector("#email").value,
+        "phone": document.querySelector("#phone").value,
+        "agree": document.querySelector("#agree").value,
+      }),  
+
+    })
+    .then((result) => {
+      return result.json();
+    })
+
+    .then((data) => {
+      document.querySelector(".form").reset();
+      console.log(data);
+  })
+
+    .catch((result) => {
+      console.log(result);
+    })
+
+  })
+
 });
